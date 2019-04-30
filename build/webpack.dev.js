084fde528aa73f10a39482a2d3d89bc0c844b501
Evan N <dacsang97@gmail.com>
2019-04-30 15:19:09 +0700

    feat: update webpack to v4, reduce bundle size by terser

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,24 +1,35 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   entry: {
-    'image-effect': './example/app.js',
+    "image-effect": "./example/app.js"
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: '/dist/',
-    filename: '[name].js'
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/dist/",
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".js", ".vue"]
   },
   module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      }
+    ]
   },
-  devtool: 'eval-source-map'
-}
+  devtool: "eval-source-map",
+  plugins: [new VueLoaderPlugin()]
+};
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,40 +1,49 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-    'image-effect': './src/index.js',
+    "image-effect": "./src/index.js"
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: '/dist/',
-    filename: '[name].min.js',
-    library: 'VueImageEffect',
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/dist/",
+    filename: "[name].min.js",
+    library: "VueImageEffect",
+    libraryTarget: "umd",
     umdNamedDefine: true
   },
   module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
-  },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      include: /\.min\.js$/,
-      compress: {
-        warnings: false
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
       }
-    })
-  ],
-  devtool: 'source-map'
-}
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        }
+      })
+    ]
+  },
+  plugins: [new VueLoaderPlugin()],
+  devtool: "source-map"
+};
