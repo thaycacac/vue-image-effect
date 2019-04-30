974d95af1e2b73eaa3ee992cf7a9a5ab0acbab58
Evan N <dacsang97@gmail.com>
2019-04-30 15:43:03 +0700

    remove useless code

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -26,7 +26,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
+        use: [
+          "vue-style-loader",
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          "postcss-loader"
+        ]
       }
     ]
   },
--- src/ImageEffect.vue
+++ src/ImageEffect.vue
@@ -23,11 +23,7 @@
     ]"
   >
     <div class="wrap-image" :class="classImage">
-      <img
-        :src="src"
-        :style="`width: ${width}; height: ${height}`"
-        :alt="alt"
-      />
+      <img :src="src" :style="`width: ${width}; height: ${height}`" :alt="alt">
     </div>
     <figcaption :class="classDescription">{{ description }}</figcaption>
   </figure>
@@ -152,152 +148,115 @@ export default {
 
 /* Zoom In */
 .zoom-in .wrap-image img {
-  -webkit-transform: scale(1);
   transform: scale(1);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .zoom-in .wrap-image:hover img {
-  -webkit-transform: scale(1.3);
   transform: scale(1.3);
 }
 
 /* Zoom Out */
 .zoom-out .wrap-image img {
-  -webkit-transform: scale(1.5);
   transform: scale(1.5);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .zoom-out .wrap-image:hover img {
-  -webkit-transform: scale(1);
   transform: scale(1);
 }
 
 /* Rotate */
 .rotate .wrap-image img {
-  -webkit-transform: rotate(15deg) scale(1.4);
   transform: rotate(15deg) scale(1.4);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .rotate .wrap-image:hover img {
-  -webkit-transform: rotate(0) scale(1);
   transform: rotate(0) scale(1);
 }
 
 /* Rotate Reverse */
 .rotate-reverse .wrap-image img {
-  -webkit-transform: rotate(0) scale(1);
   transform: rotate(0) scale(1);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .rotate-reverse .wrap-image:hover img {
-  -webkit-transform: rotate(15deg) scale(1.4);
   transform: rotate(15deg) scale(1.4);
 }
 
 /* Blur */
 .blur .wrap-image img {
-  -webkit-filter: blur(3px);
   filter: blur(3px);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .blur .wrap-image:hover img {
-  -webkit-filter: blur(0);
   filter: blur(0);
 }
 
 /* Blur Reverse*/
 .blur-reverse .wrap-image img {
-  -webkit-filter: blur(0);
   filter: blur(0);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .blur-reverse .wrap-image:hover img {
-  -webkit-filter: blur(3px);
   filter: blur(3px);
 }
 
 /* Gray Scale */
 .gray-scale .wrap-image img {
-  -webkit-filter: grayscale(100%);
   filter: grayscale(100%);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .gray-scale .wrap-image:hover img {
-  -webkit-filter: grayscale(0);
   filter: grayscale(0);
 }
 
 /* Gray Scale Reverse*/
 .gray-scale-reverse .wrap-image img {
-  -webkit-filter: grayscale(0);
   filter: grayscale(0);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .gray-scale-reverse .wrap-image:hover img {
-  -webkit-filter: grayscale(100%);
   filter: grayscale(100%);
 }
 
 /* Sepia */
 .sepia .wrap-image img {
-  -webkit-filter: sepia(100%);
   filter: sepia(100%);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .sepia .wrap-image:hover img {
-  -webkit-filter: sepia(0);
   filter: sepia(0);
 }
 
 /* Sepia Reverse*/
 .sepia-reverse .wrap-image img {
-  -webkit-filter: sepia(100%);
   filter: sepia(100%);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .sepia-reverse .wrap-image:hover img {
-  -webkit-filter: sepia(0);
   filter: sepia(0);
 }
 
 /* Blur Gray */
 .blur-gray .wrap-image img {
-  -webkit-filter: grayscale(0) blur(0);
   filter: grayscale(0) blur(0);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .blur-gray .wrap-image:hover img {
-  -webkit-filter: grayscale(100%) blur(3px);
   filter: grayscale(100%) blur(3px);
 }
 
 /* Blur Gray Reverse*/
 .blur-gray-reverse .wrap-image img {
-  -webkit-filter: grayscale(100%) blur(3px);
   filter: grayscale(100%) blur(3px);
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .blur-gray-reverse .wrap-image:hover img {
-  -webkit-filter: grayscale(0) blur(0);
   filter: grayscale(0) blur(0);
 }
 
 /* Opacity */
 .opacity .wrap-image img {
   opacity: 1;
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .opacity .wrap-image:hover img {
@@ -307,8 +266,6 @@ export default {
 /* Opacity Reverse*/
 .opacity-reverse .wrap-image img {
   opacity: 0.5;
-
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .opacity-reverse .wrap-image:hover img {
@@ -321,7 +278,6 @@ export default {
 }
 .filter-blue .wrap-image img {
   opacity: 1;
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .filter-blue .wrap-image:hover img {
@@ -334,7 +290,6 @@ export default {
 }
 .filter-blue-reverse .wrap-image img {
   opacity: 0.5;
-  -webkit-transition: 0.3s ease-in-out;
   transition: 0.3s ease-in-out;
 }
 .filter-blue-reverse .wrap-image:hover img {
@@ -344,16 +299,7 @@ export default {
 /* Flash */
 .flash .wrap-image:hover img {
   opacity: 1;
-  -webkit-animation: flash 1.5s;
   animation: flash 1.5s;
-}
-@-webkit-keyframes flash {
-  0% {
-    opacity: 0.4;
-  }
-  100% {
-    opacity: 1;
-  }
 }
 @keyframes flash {
   0% {
@@ -377,27 +323,15 @@ export default {
   content: "";
   width: 50%;
   height: 100%;
-  background: -webkit-linear-gradient(
-    left,
-    rgba(255, 255, 255, 0) 0%,
-    rgba(255, 255, 255, 0.3) 100%
-  );
   background: linear-gradient(
     to right,
     rgba(255, 255, 255, 0) 0%,
     rgba(255, 255, 255, 0.3) 100%
   );
-  -webkit-transform: skewX(-25deg);
   transform: skewX(-25deg);
 }
 .shine .wrap-image:hover::before {
-  -webkit-animation: shine 0.75s;
   animation: shine 0.75s;
-}
-@-webkit-keyframes shine {
-  100% {
-    left: 125%;
-  }
 }
 @keyframes shine {
   100% {
@@ -419,26 +353,11 @@ export default {
   height: 0;
   background: rgba(255, 255, 255, 0.2);
   border-radius: 100%;
-  -webkit-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
   opacity: 0;
 }
 .circle .wrap-image:hover::before {
-  -webkit-animation: circle 0.75s;
   animation: circle 0.75s;
-}
-@-webkit-keyframes circle {
-  0% {
-    opacity: 1;
-  }
-  40% {
-    opacity: 1;
-  }
-  100% {
-    width: 200%;
-    height: 200%;
-    opacity: 0;
-  }
 }
 @keyframes circle {
   0% {
